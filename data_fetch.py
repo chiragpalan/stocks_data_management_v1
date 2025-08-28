e8c7103951d713771f047b703b90c861e81ea781
chirag <61842354+chiragpalan@users.noreply.github.com>
2025-08-28 20:19:42 +0530

    Update data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -41,8 +41,9 @@ def init_db():
     cursor = conn.cursor()
 
     for stock in STOCKS:
+        table_name = stock.replace(".NS", "")
         cursor.execute(f"""
-            CREATE TABLE IF NOT EXISTS '{stock}' (
+            CREATE TABLE IF NOT EXISTS '{table_name}' (
                 datetime TEXT PRIMARY KEY,
                 open REAL,
                 high REAL,
