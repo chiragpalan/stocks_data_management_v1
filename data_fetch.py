463d804229607aa0d91a7afa3a673875708e0807
chirag <61842354+chiragpalan@users.noreply.github.com>
2025-09-01 22:47:52 +0530

    Update data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -104,7 +104,7 @@ def fetch_stock_data(stock):
         # Convert timezone to IST
         df["Datetime"] = df["Datetime"].dt.tz_convert(IST)
         df["volume"] = pd.to_numeric(df["volume"], errors="coerce").fillna(0).astype(int)
-        print(df.head(20))
+        print(df.head(2))
 
         df.rename(columns={
             "Datetime": "datetime",
